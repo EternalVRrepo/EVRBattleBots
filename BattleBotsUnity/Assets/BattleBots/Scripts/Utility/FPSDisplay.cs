d0515ff3e1f0ee97145e08275b6300505821abcf
Devin Scott <devinscott@eternalvr.com>
2015-03-22 23:19:29 -0500

    Added bloom to the camera, new hexagon test.

---
+++ BattleBotsUnity/Assets/BattleBots/Scripts/Utility/FPSDisplay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FPSDisplay : MonoBehaviour
+{
+	public Text FPS;
+	float deltaTime = 0.0f;
+		
+	void Update ()
+	{
+		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+	}
+		
+	void OnGUI ()
+	{
+		int w = Screen.width, h = Screen.height;
+		float msec = deltaTime * 1000.0f;
+		float fps = 1.0f / deltaTime;
+		FPS.text = string.Format ("{0:0.0} ms ({1:0.} fps)", msec, fps);
+	}
+}
