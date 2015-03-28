63764f2dc57c632a0b5fc413aa4dedfb285db7d7
mnholbart <mnholbart@gmail.com>
2015-03-28 04:59:50 -0700

    Updated MapEditor stuff, left out the combat things since I backtracked to the editor and haven't made show worthy progress

---
+++ BattleBotsUnity/Assets/MorganMerge/Scripts/HexagonData.cs
@@ -0,0 +1,33 @@
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+//	HexagonData.cs
+//	© EternalVR, All Rights Reserved
+//
+//	description:	Class used to store information about a hexagon, used to save/load
+//					premade maps
+//
+//	authors:		Morgan Holbart
+//
+///////////////////////////////////////////////////////////////////////////////// 
+
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class HexagonData : ScriptableObject {
+	public int HexRow;
+	public int HexColumn;
+	public Hexagon.HexType CurrentHexType;
+	public Material CurrentBrushTexture;
+	public int HexHeight;
+	public Material[] typeMaterials; //List of materials
+
+	public HexagonData(Hexagon h) {
+		HexRow = h.HexRow;
+		HexHeight = h.HexHeight;
+		CurrentHexType = h.CurrentHexType;
+		CurrentBrushTexture = h.CurrentBrushTexture;
+		typeMaterials = h.typeMaterials;
+		HexHeight = h.HexHeight;
+	}
+}
