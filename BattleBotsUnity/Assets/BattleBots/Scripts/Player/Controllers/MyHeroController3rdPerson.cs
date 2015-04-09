14a6406b8c28784d15489a1aa08b79ddb056a260
Devin Scott <devinscott@eternalvr.com>
2015-04-08 21:19:35 -0500

    Asset pipeline.

--- BattleBotsUnity/Assets/BattleBots/Scripts/Player/Controllers/MyHeroController3rdPerson.cs
+++ BattleBotsUnity/Assets/BattleBots/Scripts/Player/Controllers/MyHeroController3rdPerson.cs
@@ -10,6 +10,7 @@ public class MyHeroController3rdPerson : MonoBehaviour
 {
 	public float forwardSpeed = 3f, jumpSpeed = 15f;
 	public Transform cameraController;
+	public Transform OVRCamera, OVRCenterAnchor;
 	public Transform cameraRig;
 	float airSpeed;
 //	float airSpeed, backwardSpeed, strafeSpeed;
@@ -24,43 +25,26 @@ public class MyHeroController3rdPerson : MonoBehaviour
 	
 	void Start ()
 	{
+		//Get Components
 		character = transform.GetComponent<CharacterController> ();
 		animator = transform.GetComponentInChildren<Animator> ();
+
 		//Set Speeds
 		airSpeed = forwardSpeed * 0.8f;
 //		backwardSpeed = forwardSpeed * 0.5f;
 //		strafeSpeed = forwardSpeed * 0.75f;
 
+		//Set Up Rig
 		if (thirdPersonCameraLockPivot == null) {
 			thirdPersonCameraDummyObject = new GameObject ("thirdPersonCameraDummyObject"); //Create Dummy Object
 			thirdPersonCameraLockPivot = new GameObject ("thirdPersonCameraLockPivot"); //Create Pivot
 			thirdPersonCameraLockPivot.transform.position = transform.position + new Vector3 (0, character.height / 2, 0); //Set Pivot in center of the character
 			cameraRig.position = new Vector3 (transform.position.x, transform.position.y + 2, transform.position.z - 4f); //Move Camera To Perspective
-//			cameraController.LookAt (transform.position); //Look At Player
-//			cameraController.eulerAngles = new Vector3 (0, cameraController.eulerAngles.y, cameraController.eulerAngles.z); //Even Out Ground Plane
 			cameraRig.parent = thirdPersonCameraLockPivot.transform; //Parents Camera to Pivot
 		}
 	}
-	
-	void Update ()
-	{
-		xJoystick = Input.GetAxis ("Horizontal");
-		yJoystick = Input.GetAxis ("Vertical");
 
-		if (Mathf.Round (xJoystick) != 0 || Mathf.Round (yJoystick) != 0) 
-			animator.SetBool ("Walking", true);
-		else
-			animator.SetBool ("Walking", false);
-
-		Jump ();
-	}
-
-	void FixedUpdate ()
-	{
-		CameraSetUp ();
-		Move ();
-	}
-
+	//The Camera Rig
 	void CameraSetUp ()
 	{
 		//Keep Pivot On Character's Center
@@ -72,23 +56,26 @@ public class MyHeroController3rdPerson : MonoBehaviour
 		thirdPersonCameraDummyObject.transform.eulerAngles = new Vector3 (0, thirdPersonCameraDummyObject.transform.eulerAngles.y, 0); //Removed Roll & Tilt
 	}
 
-	void Jump ()
+	void FixedUpdate ()
 	{
-		//Check for jump
-		if (character.isGrounded) {
-			if (Input.GetButtonDown ("Jump")) {
-				animator.SetTrigger ("Jump");
-				velocity = character.velocity;
-				velocity.y = jumpSpeed;
-			}
-		} else {
-//						//Apply gravity to our velocity to diminish it over time
-			velocity.y += Physics.gravity.y * Time.deltaTime;
-			
-			//Movement in-air
-			movement.x *= airSpeed;
-			movement.z *= airSpeed;
-		}
+		CameraSetUp ();
+		Move ();
+	}
+
+	void Update ()
+	{
+		//Collect Data
+		xJoystick = Input.GetAxis ("Horizontal");
+		yJoystick = Input.GetAxis ("Vertical");
+
+		//Animation
+		if (Mathf.Round (xJoystick) != 0 || Mathf.Round (yJoystick) != 0) 
+			animator.SetBool ("Walking", true);
+		else
+			animator.SetBool ("Walking", false);
+
+		//Check for Jump
+		Jump ();
 	}
 
 	void Move ()
@@ -119,4 +106,23 @@ public class MyHeroController3rdPerson : MonoBehaviour
 		if (character.isGrounded)
 			velocity = Vector3.zero;
 	}	
+	
+	void Jump ()
+	{
+		//Check for jump
+		if (character.isGrounded) {
+			if (Input.GetButtonDown ("Jump")) {
+				animator.SetTrigger ("Jump");
+				velocity = character.velocity;
+				velocity.y = jumpSpeed;
+			}
+		} else {
+			//						//Apply gravity to our velocity to diminish it over time
+			velocity.y += Physics.gravity.y * Time.deltaTime;
+			
+			//Movement in-air
+			movement.x *= airSpeed;
+			movement.z *= airSpeed;
+		}
+	}
 }
--- BattleBotsUnity/Assets/MorganMerge/Scripts/GameManager.cs
+++ BattleBotsUnity/Assets/MorganMerge/Scripts/GameManager.cs
@@ -13,29 +13,33 @@
 using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
 	public static GameManager instance;
-	public CombatManager combatManager;
+//	public CombatManager combatManager;
 
-	void Awake() {
+	void Awake ()
+	{
 		instance = this;
-		DontDestroyOnLoad(this);
+		DontDestroyOnLoad (this);
 	}
 
 	/// <summary>
 	/// 
 	/// </summary>
-	void Update() {
+	void Update ()
+	{
 		if (Input.GetKeyDown (KeyCode.T)) {
-			StartCombat();
+			StartCombat ();
 		}
 	}
 
 	/// <summary>
 	/// Called to switch to and start combat
 	/// </summary>
-	protected void StartCombat() {
+	protected void StartCombat ()
+	{
 		Debug.Log ("Loading Level: CombatTest"); 
 		Application.LoadLevel ("CombatTest");
 
@@ -44,10 +48,11 @@ public class GameManager : MonoBehaviour {
 	/// <summary>
 	/// Raises the level was loaded event.
 	/// </summary>
-	void OnLevelWasLoaded(int level) {
+	void OnLevelWasLoaded (int level)
+	{
 		if (Application.loadedLevelName == "CombatTest") {
 //			combatManager = GameObject.Find ("CombatManager").GetComponent<CombatManager>();
-			BoardManager.instance.InitializeMapForCombat(0);
+			BoardManager.instance.InitializeMapForCombat (0);
 //			combatManager.SetupCombat(0);
 		}
 	}
